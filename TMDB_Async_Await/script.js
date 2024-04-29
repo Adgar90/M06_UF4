636bb8704125fb54858d1aa123ae4601d2c8e7ef
unknown <adriagp90@gmail.com>
2024-04-29 18:10:39 +0200

    finalitzat exercici

--- TMDB_Async_Await/script.js
+++ TMDB_Async_Await/script.js
@@ -4,24 +4,81 @@ const keys = {
     session_id: '',
     account_id: ''
 }
-
+const token_auth = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMThhZmZjNTJhZTFmODZkNzZhOTUzMWZkZGNkNDc4YSIsInN1YiI6IjY2MWQ1ODFkNGNhNjc2MDEzMTFiYmFkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dO_-pvV7vqs53u1bB4-ffG5Ik5gMAHdKQL1kOIj8_vQ';
+let total_pages = 0;
+let current_page = 1;
+let current_query = "";
 let moviesResult = document.getElementById("moviesResult");
 
+window.addEventListener('scroll', () => {
+    const {scrollTop, scrollHeight,clientHeight} =
+    document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 1 &&
+    current_page<total_pages) {
+        current_page+=1;
+        searchMovies(current_query);
+    }
+});
 
-function setFav(id, favBool){
+async function setFav(id, favBool){
     moviesResult.innerHTML="";
-
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            Authorization: token_auth
+        },
+        body: JSON.stringify({media_type: 'movie', media_id: id, favorite: favBool})
+    };
+    let url = 'https://api.themoviedb.org/3/account/21215434/favorite';
+    await fetch(url, options);
     showFavs();
 }
 
-function showFavs(){
+async function showFavs(){
     moviesResult.innerHTML="";
-
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: token_auth
+        }
+    };
+    let url = 'https://api.themoviedb.org/3/account/21215434/favorite/movies';
+    let response = await fetch(url, options);
+    let data = await response.json();
+    console.log(data.results);
+    data.results.forEach(movie => printMovie(movie, true, false));
 }
 
-function searchMovies(query){
+async function searchMovies(query){
     clearInput();
     removeActive();
+    if (current_query != query) {
+        current_page = 1;
+        moviesResult.innerHTML="";
+    }
+    current_query = query;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: token_auth
+        }
+    };
+    let url = `https://api.themoviedb.org/3/search/movie?query=${query}&page=${current_page}`;
+    let response = await fetch(url, options);
+    let data = await response.json();
+    total_pages = data.total_pages;
+    console.log(data.total_pages);
+    data.results.forEach( async movie  => {
+        let url = `https://api.themoviedb.org/3/movie/${movie.id}/account_states`;
+        let response = await fetch(url, options);
+        let data = await response.json();
+        console.log(data.favorite);
+        if (!data.favorite) printMovie(movie, false, false);
+    });
 }
 
 
@@ -32,7 +89,6 @@ function searchMovies(query){
 document.getElementById("showFavs").addEventListener("click", function(){
     removeActive();
     this.classList.add("active");
-
     showFavs();
 })
 
@@ -40,7 +96,6 @@ document.getElementById("showFavs").addEventListener("click", function(){
 document.getElementById("showWatch").addEventListener("click", function(){
     removeActive();
     this.classList.add("active");
-
     //showWatch();
 })
 
